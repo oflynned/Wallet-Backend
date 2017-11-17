aa74a4c6a0fa7deed1319eb7505269969156524a
Ed <oflynned@tcd.ie>
2017-11-17 10:35:16 +0000

    added mlab backend

--- app/__init__.py
+++ app/__init__.py
@@ -18,5 +18,11 @@ app.register_blueprint(message, url_prefix="/api/v1/message")
 app.register_blueprint(transaction_endpoint, url_prefix="/api/v1/transaction")
 app.register_blueprint(user, url_prefix="/api/v1/user")
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plynk"
+if "MONGODB_URI" in os.environ:
+    app.config["MONGO_URI"] = os.environ["MONGODB_URI"] + "/plynk"
+    print("Using remote server")
+else:
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/loinnir"
+    print("Using local server")
+
 mongo = PyMongo(app)
